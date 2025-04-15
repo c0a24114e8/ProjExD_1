a631cb21066059db202e90efec2d125df97512f1
c0a24114 <coa24114e8@edu.teu.ac.jp>
2025-04-15 16:21:59 +0900

    Practice1-9

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -12,15 +12,28 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.image.load("fig/3.png")
     kk_img = pg.transform.flip(kk_img, True, False)#練習2
-    bg_img2 = pg.transform.flip(bg_img2, True, False)#練習8
+    bg_img2 = pg.transform.flip(bg_img, True, False)#練習8
+    kk_rct = kk_img.get_rect()
+    kk_rct.center = 300, 200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [-tmr, 0])#練習6
-        screen.blit(bg_img2, [1600-tmr, 0])#練習7
-        screen.blit(kk_img, [300, 200])#練習4
+        
+        x = tmr%3200#練習9
+        screen.blit(bg_img, [-x, 0])#練習6
+        screen.blit(bg_img2, [1600-x, 0])#練習7
+        screen.blit(bg_img, [3200-x, 0])
+        screen.blit(kk_img, kk_rct)#練習4
+        key_lst = pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            kk_rct.move_ip((0, -1))
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip((0, 1))
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((1, 0))
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip((-1, 0))
         pg.display.update()
         tmr += 1        
         clock.tick(200)#練習5
