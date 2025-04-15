e4e7966e5a5d27e68223695399a6feccbbae0b32
c0a24114 <coa24114e8@edu.teu.ac.jp>
2025-04-15 16:51:06 +0900

    Exercise1-2

--- ex1/pygame_image.py
+++ ex1/pygame_image.py
@@ -16,6 +16,8 @@ def main():
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
     tmr = 0
+    a = 0
+    b = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
@@ -27,15 +29,17 @@ def main():
         screen.blit(kk_img, kk_rct)#練習4
         key_lst = pg.key.get_pressed()
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip((0, -1))
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip((0, 1))
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip((2, 0))#演習1-2
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((-1, 0))#練習10
+            b = -1
+        elif key_lst[pg.K_DOWN]:
+            b = 1
+        elif key_lst[pg.K_RIGHT]:
+            a = 1
+        elif key_lst[pg.K_LEFT]:
+            a = -1
         else:
-            kk_rct.move_ip((-1, 0))#演習1-1
+            a = -1
+            b = 0
+        kk_rct.move_ip((a, b))#演習1-2
         pg.display.update()
         tmr += 1        
         clock.tick(200)#練習5
